f22a9f65959c76ce01608b7ef3c9b47a74ee0911
giordanoale <a.giordano89@gmail.com>
2021-04-09 12:40:19 +0200

    Implementata ricerca, i risultati vengono visualizzati nella console

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
 var app = new Vue ({
     el: "#app",
     data: {
-        
+        titoloFilm:"",
+    },
+    methods: {
+        search: function() {
+            axios.get('https://api.themoviedb.org/3/search/movie?', {
+                params: {
+                    api_key: "e7ecbc05df37bdc5e5aa48a01b375770",
+                    query: this.titoloFilm,
+                    language: "it-IT",
+                }
+            })
+            .then((response) => {
+                console.log(response.data);
+            })
+        }
     }
 }) 
