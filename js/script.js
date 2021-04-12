913bdfcf44fe2241fee876febf53925dcf48e921
giordanoale <a.giordano89@gmail.com>
2021-04-12 17:48:41 +0200

    Aggiunta integrazione della ricerca di serie TV  e compatibilità con altre lingue

--- js/script.js
+++ js/script.js
@@ -3,26 +3,43 @@ var app = new Vue ({
     data: {
         titoloFilm:"",
         films: [],
+        series:[],
     },
     methods: {
-        search: function() {
-            axios.get('https://api.themoviedb.org/3/search/movie', {
-                params: {
-                    api_key: "e7ecbc05df37bdc5e5aa48a01b375770",
-                    query: this.titoloFilm,
-                    language: "it-IT",
-                }
-            })
-            .then((response) => {
-                // console.log(response.data);
-                this.films = response.data.results;
-                for (var i = 0; i < this.films.length; i++) {
-                    this.films[i].vote_average = Math.ceil(this.films[i].vote_average / 2);
-                };
-            
-                console.log(this.films);
+        search: 
+        
+            function(movies) {
+                axios.get('https://api.themoviedb.org/3/search/movie', {
+                    params: {
+                        api_key: "e7ecbc05df37bdc5e5aa48a01b375770",
+                        query: this.titoloFilm,
+                        language: "it-IT",
+                    }
+                })
+                .then((response) => {
+                    this.films = response.data.results;
+                    for (var i = 0; i < this.films.length; i++) {
+                        this.films[i].vote_average = Math.ceil(this.films[i].vote_average / 2);
+                    };
+                
 
-            })
+                })
+
+                axios.get('https://api.themoviedb.org/3/search/tv', {
+                    params: {
+                        api_key: "e7ecbc05df37bdc5e5aa48a01b375770",
+                        query: this.titoloFilm,
+                        language: "it-IT",
+                    }
+                })
+                .then((response) => {
+                    this.series = response.data.results;
+                    for (var i = 0; i < this.series.length; i++) {
+                        this.series[i].vote_average = Math.ceil(this.series[i].vote_average / 2);
+                    };
+                
+                    console.log(this.series);
+                })
 
             this.titoloFilm = "";
         },
